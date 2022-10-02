d2d84fc3262906cd4fdef0acd198dbbec3528c2b
vbatista <valdeir.neto@gmail.com>
2022-10-01 22:44:07 -0300

    full project skeleton

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from api import users, courses, sections
+from db.db_setup import engine
+from db.models import user, course
 
+user.Base.metadata.create_all(bind=engine)
+course.Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app = FastAPI(
+    title="Fast API LMS",
+    description="LMS for managing students and courses.",
+    version="0.0.1",
+    contact={
+        "name": "Gwen",
+        "email": "gwen@example.com",
+    },
+    license_info={
+        "name": "MIT",
+    },
+)
+
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(sections.router)
