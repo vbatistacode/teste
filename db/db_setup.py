7d4b6d45fd8d9995543648d01e2a76b59e99f5c1
vbatista <valdeir.neto@gmail.com>
2022-10-01 23:12:48 -0300

    db conn string setup

--- db/db_setup.py
+++ db/db_setup.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://gwen@localhost/fast_lms"
-ASYNC_SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://gwen@localhost/fast_lms"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://meshdb_user:Boqgg3dljhOPzTFDl4aA2W8aoxnXAGzw@dpg-ccrieq9a6gdl22bkim20-a.oregon-postgres.render.com/meshdb"
+ASYNC_SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://meshdb_user:Boqgg3dljhOPzTFDl4aA2W8aoxnXAGzw@dpg-ccrieq9a6gdl22bkim20-a.oregon-postgres.render.com/meshdb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
 async_engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL)
